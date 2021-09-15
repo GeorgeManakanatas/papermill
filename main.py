3ed444b2b74aad46165f9599a1648b7f3e951861
GeorgeManakanatas <manakanatas@gmail.com>
2021-09-15 22:20:42 +0200

    Many changes later

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import papermill as pm
 
-params1 = {"target_drive":"I","sound_file":"beep-01a.wav","output_path":"outputs"}
-pm.execute_notebook(
-   'drive_map_analysis.ipynb',
-   'outputs/drive_map_analysis_output1.ipynb',
-   parameters = params1
-)
+# params1 = {"target_drive":"I","sound_file":"beep-01a.wav","output_path":"outputs"}
+# pm.execute_notebook(
+#    'drive_map_analysis.ipynb',
+#    'outputs/drive_map_analysis_output1.ipynb',
+#    parameters = params1
+# )
 
-params2 = {"target_drive":"C","sound_file":"beep-01a.wav","output_path":"outputs"}
-pm.execute_notebook(
-   'drive_map_analysis.ipynb',
-   'outputs/drive_map_analysis_output2.ipynb',
-   parameters = params2
-)
+# params2 = {"target_drive":"C","sound_file":"beep-01a.wav","output_path":"outputs"}
+# pm.execute_notebook(
+#    'drive_map_analysis.ipynb',
+#    'outputs/drive_map_analysis_output2.ipynb',
+#    parameters = params2
+# )
 
-params3 = {"target_drive":"I","sound_file":"beep-01a.wav","output_path":"outputs"}
+params3 = {"base_path":"Y:","target_drive":"\\Unit_SCT","sound_file":"beep-01a.wav","output_path":"C:\\Users\\manakang\\Videos\\Repos\\papermill\\outputs"}
 pm.execute_notebook(
    'drive_map.ipynb',
    'outputs/drive_map_output3.ipynb',
    parameters = params3
 )
 
-params4 = {"target_drive":"C","sound_file":"beep-01a.wav","output_path":"outputs"}
-pm.execute_notebook(
-   'drive_map.ipynb',
-   'outputs/drive_map_output4.ipynb',
-   parameters = params4
-)
+# params4 = {"target_drive":"C","sound_file":"beep-01a.wav","output_path":"outputs"}
+# pm.execute_notebook(
+#    'drive_map.ipynb',
+#    'outputs/drive_map_output4.ipynb',
+#    parameters = params4
+# )
